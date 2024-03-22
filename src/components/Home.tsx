5292a5dc06ccae1cd715805b31f8c3200918b805
HasithKovinda <hasithkovinda@outlook.com>
2024-03-22 21:21:21 +0530

    add typesafe custom hook for the data loading

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,9 @@
 import Events from "./Home/Events";
 import Favorite from "./Home/Favorite";
-import Footer from "./Footer";
 import Restaurants from "./Home/Restaurants";
 import Service from "./Home/Service";
 import Subscribe from "./Home/Subscribe";
 import Hero from "./Home/Hero";
-import NavBar from "./NavBar";
 import Works from "./Home/Works";
 
 function Home() {
--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,5 +1,16 @@
+import useLoaderData from "../../hooks/useDataLoader";
+import { getMenu } from "../../services/apiRestaurant";
+import { MenuType } from "../../types/pizza";
+
 function Menu() {
+  const data = useLoaderData<MenuType[]>();
+  console.log("🚀 ~ Menu ~ data:", data);
   return <div>Menu</div>;
+}
+
+export async function loader(): Promise<MenuType[]> {
+  const results = await getMenu();
+  return results;
 }
 
 export default Menu;
