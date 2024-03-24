62e4bb7ff5564e8d4ff90e9441be8e2342dfb2e6
HasithKovinda <hasithkovinda@outlook.com>
2024-03-24 23:51:52 +0530

    fixed stylig issue in navbar

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,7 +2,6 @@ import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Loading from "./Loading";
-import Model from "./Model";
 
 function AppLayout() {
   const navigation = useNavigation();
@@ -10,7 +9,6 @@ function AppLayout() {
 
   return (
     <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
-      {/* <Model /> */}
       {isLoading && <Loading />}
       <NavBar />
       <Outlet />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,63 +13,66 @@ function NavBar() {
   );
   const [showSideBar, setShowSideBar] = useState(false);
   return (
-    <nav className="md:flex md:items-center md:justify-between max-w-screen-2xl md:mx-auto px-12  py-3">
-      <div className="flex items-center justify-between py-4 md:p-0">
-        <h1 className="font-sans text-3xl font-bold tracking-wide uppercase">
-          quick<span className="text-primary">eat</span>
-        </h1>
-        <BiMenu
-          className="text-primary text-4xl cursor-pointer hover:scale-110 transition-all md:hidden"
-          onClick={() => setShowSideBar(true)}
-        />
-      </div>
-      <ul className="hidden  md:flex md:px-4 md:text-lg md:tracking-widest md:uppercase md:gap-x-2 lg:gap-x-14">
-        <NavLink to="/">Home </NavLink>
-        <NavLink to="/menu">Menu </NavLink>
-        <NavLink to="/contact">Contact </NavLink>
-        {totalQuantity ? (
-          <NavLink to="/cart" classNames="flex text-primary gap-3">
-            {" "}
-            <span>Cart</span>
-            <BiCart className="text-2xl" />
-            <span>{totalQuantity}</span>
-          </NavLink>
-        ) : null}
-      </ul>
-      <div className="flex items-center text-zinc-600 gap-2">
-        <SearchOrder />
-        {userName && (
-          <span className="text-2xl font-semibold capitalize">{userName}</span>
-        )}
-      </div>
-      <aside
-        className={`min-h-screen w-full fixed top-0 left-0 bg-slate-100 ${showSideBar ? "translate-x-0" : "translate-x-[-100%]"} ease-linear transition-all duration-300`}
-      >
-        <div className="flex items-center justify-between p-4 mb-3">
+    <header>
+      <nav className="md:flex md:items-center md:justify-between max-w-screen-2xl md:mx-auto px-12  py-3">
+        <div className="flex items-center justify-between py-4 md:p-0">
           <h1 className="font-sans text-3xl font-bold tracking-wide uppercase">
             quick<span className="text-primary">eat</span>
           </h1>
-          <BiXCircle
-            className="text-red-600 text-4xl cursor-pointer hover:text-red-700 transition-all"
-            onClick={() => setShowSideBar(false)}
+          <BiMenu
+            className="text-primary text-4xl cursor-pointer hover:scale-110 transition-all md:hidden"
+            onClick={() => setShowSideBar(true)}
           />
         </div>
-        <ul className="flex flex-col  px-4">
-          <li>
-            <Link name="Home" to="/" />
-          </li>
-          <li>
-            <Link name="About" to="about" />
-          </li>
-          <li>
-            <Link name="Contact" to="contact" />
-          </li>
-          <li>
-            <Link name="Order" to="order" />
-          </li>
+        <ul className="hidden  md:flex md:px-4 md:text-lg md:tracking-widest md:uppercase md:gap-x-2 lg:gap-x-14">
+          <NavLink to="/">Home </NavLink>
+          <NavLink to="/menu">Menu </NavLink>
+          <NavLink to="/contact">Contact </NavLink>
+          {totalQuantity ? (
+            <NavLink to="/cart" classNames="flex text-primary gap-3">
+              <span>Cart</span>
+              <BiCart className="text-2xl" />
+              <span>{totalQuantity}</span>
+            </NavLink>
+          ) : null}
         </ul>
-      </aside>
-    </nav>
+        <div className="flex items-center text-zinc-600 gap-2">
+          <SearchOrder />
+          {userName && (
+            <span className="text-2xl font-semibold capitalize">
+              {userName}
+            </span>
+          )}
+        </div>
+        <aside
+          className={`min-h-screen w-full fixed top-0 left-0 bg-slate-100 ${showSideBar ? "translate-x-0" : "translate-x-[-100%]"} ease-linear transition-all duration-300`}
+        >
+          <div className="flex items-center justify-between p-4 mb-3">
+            <h1 className="font-sans text-3xl font-bold tracking-wide uppercase">
+              quick<span className="text-primary">eat</span>
+            </h1>
+            <BiXCircle
+              className="text-red-600 text-4xl cursor-pointer hover:text-red-700 transition-all"
+              onClick={() => setShowSideBar(false)}
+            />
+          </div>
+          <ul className="flex flex-col  px-4">
+            <li>
+              <Link name="Home" to="/" />
+            </li>
+            <li>
+              <Link name="About" to="about" />
+            </li>
+            <li>
+              <Link name="Contact" to="contact" />
+            </li>
+            <li>
+              <Link name="Order" to="order" />
+            </li>
+          </ul>
+        </aside>
+      </nav>
+    </header>
   );
 }
 
